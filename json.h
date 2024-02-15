b3413f72568fa1972f2b8bc8bf3760a37d3db92b
Daniel C <brikbusters@gmail.com>
2024-02-15 00:58:44 -0500

    Add some comments to struct Parse fields

--- json.h
+++ json.h
@@ -1,10 +1,10 @@
 struct Parse {
-	char *b;
-	char *intent;
+	char *b; // current lexer ptr
+	char *intent; // current lexer ptr for intent
 	char intent_type;
-	char intent_key[16];
-	int intent_i;
-
+	char intent_key[16]; // key value for intent
+	int intent_i; // index for intent
+	char out_of_bounds; // will be set to 1 if out of bounds in array access
 	char type;
 	char *str;
 	int str_len;
@@ -12,5 +12,4 @@ struct Parse {
 };
 
 int json_get(struct Parse *p, char *text, char *intent);
-
 char *json_fixup_string(struct Parse *p);
